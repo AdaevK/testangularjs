3eb6f615fd5be0463dc26cc22619e9c6b77fb15b
Kirill Adaev <kinder8906@gmail.com>
2014-12-07 16:46:49 +0700

    added create adverts in seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times{ FactoryGirl.create( :advert_with_photo ) }
